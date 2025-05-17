31d190892b8fb5be5fb67e6a9dfc10a8bf36d4f5
Vignesh8205 <vigneshphr@gmail.com>
2025-05-18 02:11:48 +0530

    error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/Portfolio-Website/', // 👈 IMPORTANT
+    base: '/Portfolio-Website/',  // ✅ Add this line
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
